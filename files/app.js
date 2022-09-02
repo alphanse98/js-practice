0250760f3e93b611f4768896d20218bf926fe23f
alphanse <alphanse98@gmail.com>
2022-09-02 23:23:17 +0530

    add

---
+++ files/app.js
@@ -0,0 +1,252 @@
+let y = 'hgftt13kl786jhhj74'
+let srt = y.split('')
+let t = 0
+ 
+console.log(srt);
+srt.forEach(srt => {
+  if( srt>0 || srt<=9 ){
+    
+    t = t + (~~srt)
+  }
+});
+console.log(t);
+
+ 
+ 
+
+
+
+
+
+
+
+
+// let a = 12345
+// let y = 0
+// let srt = a.toString()
+
+// for(i=1;i<=srt.length;i++){
+//   if(i%2==0){
+//      y = y + i
+    
+//   }
+// }
+// console.log(y);
+
+
+
+
+
+// let l = "http://127.0.0.1:5501/index.html";
+// let arr = l.split("");
+
+// // console.log(arr);
+
+// arr.forEach((d) => {
+//   if (d == "a" || d == "b" || d == "c" 
+//   || d == "e" || d == "f" || d == "g"
+//   || d == "h" || d == "i" || d == "j"
+//   || d == "k" || d == "l" || d == "m"
+//   || d == "n" || d == "o" || d == "p"
+//   || d == "q" || d == "r" || d == "s"
+//   || d == "t" || d == "u" || d == "v"
+//   || d == "w" || d == "x" || d == "y" || d == "z") {
+//     console.log(d);
+//   } else {
+//     console.log("");
+//   }
+// });
+
+
+// let st = 'mam'
+// let ma = st.toString()
+// let ba = ma.split('')
+// let re = ba.reverse()
+// let join = ba.join('')
+
+// if(ma == join){
+//   console.log(ma ,"is palindrome");
+// }else{console.log(ma ,"is not palindrome");}
+
+
+
+
+
+// let myname = "alphanse";
+// let arr = myname.split("")
+// console.log(arr);
+// arr = arr.filter((data) => {return data !== 'a'});
+//   console.log(arr);
+
+// let a = "https://www.youtube.com";
+// let b = a.split(".");
+
+// let c = b[0];
+// let d = c.split(":");
+
+// let w = c.split("//");
+
+// let first = d[0];
+// console.log(first);
+
+// let sec = w[1];
+// console.log(sec);
+
+// console.log(b[1]);
+// console.log(b[2]);
+
+// let a = [1,3,2]
+// let arr = []
+// let b = 0
+
+// a.forEach(ele => {
+
+//     console.log(b)
+//     if(ele>b){
+//       b = ele
+
+//        arr.push(ele)
+
+//         }else{
+//           // console.log(ele)
+//           arr.shift()
+//         }
+// })
+// console.log(arr)
+
+// let arr = [],
+//   obj = [
+//     {
+//       id: 1,
+//       name: "mari selvam",
+//       age: "96",
+//     },
+//     {
+//       id: 2,
+//       name: "leo",
+//       age: "23",
+//     },
+//     {
+//       id: 3,
+//       name: "Rahul",
+//       age: "25",
+//     },
+//     {
+//       id: 4,
+//       name: "muthu kumar",
+//       age: "24",
+//     },
+//     {
+//       id: 5,
+//       name: "vel",
+//       age: "26",
+//     },
+//   ],
+//   index,
+//   addbtn = document.querySelector(".add"),
+//   removeBtn = document.querySelector(".remove"),
+//   userInFun = () => (usein = Number(document.querySelector(".int").value));
+//   let v = 1
+// addbtn.addEventListener("click", () => {
+//   userInFun();
+//   obj.filter((data) => {
+//     if (data.id == usein && !arr.includes(data)) {
+//       arr.push(data);
+//       console.log(arr);
+//     } else {
+
+//       if(arr.includes(data) && data.id == usein ) {
+//         v = ++v
+//         console.log(v);
+
+//         }
+
+//     }
+//   });
+// });
+// removeBtn.addEventListener("click", () => {
+//   userInFun();
+//   arr = arr.filter((data) => {return data.id !== usein;});
+//   console.log(arr);
+// });
+
+// nameFuc(3)
+
+// console.log(ab);
+// var ab = 5
+
+//Hoisting
+// var ab
+// console.log(ab);
+// var ab = 5
+
+// let indexUse;
+// let check;
+// let addbtn = document.querySelector(".add"),
+//   removeBtn = document.querySelector(".remove");
+//   addbtn.addEventListener("click", () => {
+//   operation();
+//   if (!arr.includes(obj[indexUse])) {
+//     arr.push(obj[indexUse]);
+//     console.log(arr);
+//   }
+// });
+// removeBtn.addEventListener("click", () => {
+//   operation();
+//   arr.pop(indexUse);
+//   console.log(arr);
+// });
+// let operation = () => {
+//   (usein = document.querySelector(".int").value),
+//     (indexUse = parseInt(usein - 1));
+//   return indexUse;
+// };
+
+// console.log(operation());
+// let two = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("2");
+//       resolve(mari());
+//     //   reject();
+//     }, 0);
+//   });
+// };
+// two()
+// //   .then(() => console.log("mari"))
+//   .catch(() => console.log("error mari wast"));
+
+//   let mari = ()=> console.log('mari wrking');
+
+// let one = () => console.log("1");
+
+// let two = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("2");
+//       resolve();
+//     }, 0);
+//   });
+// };
+
+// let three = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("3" , resolve,reject);
+//       resolve();
+//     }, 0);
+//   });
+// };
+
+// let f4 = async () => {
+//   two();
+//   await three();
+//   one();
+// };
+// f4();
+
+// var arcr = [1, 2, 3, 3];
+// console.log(arcr);
+// // console.log(arcr.map());
+
+// var mapx = arcr.map((v) => console.log(v));
